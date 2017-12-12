1339107cc7a60863dab9799fbee86632becd1465
Jeff Duke <jeff.duke@gmail.com>
2017-12-11 22:00:09 -0700

    post new todo to server

--- client/scripts/App.vue
+++ client/scripts/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <section>
-    <article v-for="todo in todos" :key="todo.id">
-      <h1>{{todo.description}}</h1>
-      <input type="checkbox" :checked="todo.done">
-    </article>
-  </section>
+  <main>
+    <section>
+      <label> Todo
+        <input v-model="description" placeholder="Enter a Todo" type="text"/>
+      </label>
+      <button
+        @click="addTodo"
+      >
+        Add Todo
+      </button>
+    </section>
+    <section class="todo-list">
+      <article v-for="todo in todos" class="todo-list__todo" :key="todo.id">
+        <h1>{{todo.description}}</h1>
+        <input type="checkbox" :checked="todo.done">
+      </article>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -13,6 +25,7 @@ export default {
   data() {
     return {
       todos: [],
+      description: '',
     };
   },
   mounted: function loadTodosOnMount() {
@@ -23,6 +36,30 @@ export default {
       fetch('http://localhost:8004/api/todos')
         .then(response => response.json())
         .then(data => (this.todos = data));
+    },
+
+    addTodo() {
+      const todo = {
+        description: this.description,
+        done: false,
+        id: Date.now(),
+      };
+
+      this.todos.push(todo);
+      this.storeTodoOnServer(todo);
+    },
+
+    storeTodoOnServer(todo) {
+      fetch('http://localhost:8004/api/todos', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(todo),
+      })
+        .then(res => res.json())
+        .then(res => console.log(res))
+        .catch(err => console.error('an error occurred ', err));
     },
   },
 };
