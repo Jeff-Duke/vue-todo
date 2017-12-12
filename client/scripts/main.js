f035b1737db7fdd055bdded279dbacf9c0bddb0a
Jeff Duke <jeff.duke@gmail.com>
2017-12-11 18:50:05 -0700

    render todos to screen

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -2,33 +2,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-// const renderTodos = (todos) => {
-//   const todoItems = [];
-
-//   todos.forEach(todo => {
-//     todoItems.push([
-//       '<li>',
-//         `<label for="${ todo.id }">`,
-//           `<input ${ todo.done ? 'checked' : '' } id="${ todo.id }" type="checkbox" />`,
-//           todo.description,
-//         '</label>',
-//       '</li>',
-//     ].join(''));
-//   });
-
-//   document.querySelector('#root').innerHTML = `<ul>${ todoItems.join('') }</ul>`;
-// };
-
-// const getTodos = () => {
-//   return new Promise((resolve) => {
-//     fetch('http://localhost:8004/api/todos')
-//       .then(r => r.json())
-//       .then(data => resolve(data));
-//   });
-// };
-
-// document.addEventListener('DOMContentLoaded', () => getTodos().then(renderTodos));
-
 var app = new Vue({
   el: '#root',
   render: h => h(App),
